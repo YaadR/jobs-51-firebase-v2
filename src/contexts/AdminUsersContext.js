01138f6ee77dccf487bf418a46051b476e4f882c
Y Patterns <y.code.patterns@gmail.com>
2022-01-05 19:24:59 +0200

    Creating getUsers
    Part of adding CSV print button task. yet to be finish

--- src/contexts/AdminUsersContext.js
+++ src/contexts/AdminUsersContext.js
@@ -3,6 +3,7 @@ import { useQuery, useQueryClient } from "react-query";
 import { useHistory } from "react-router-dom";
 import useToggle from "../hooks/general/useToggle";
 import qs from "query-string";
+import getUsers from "../lib/API/queries/getUsers";
 
 export const AdminUsersContext = createContext();
 
@@ -15,7 +16,17 @@ export const AdminUsersProvider = ({ children }) => {
 
 	// @Y- hookRef for user data implementation is needed -
 	// AdminActivitiesProvider under AdminActivitiesContext for example
+	/*
+		const hookRef = useQuery(["users", query], async () => {
+		const res = await getUsers(query, lastResult);
+		setLastResult(res?.lastResult);
+		const oldData = queryClient.getQueryData(["users", query]);
+		const data = Array.isArray(oldData) ? [...oldData, ...res.data] : res.data;
 
+		data?.forEach((a) => queryClient.setQueryData(["user-activity", a?.id], a));
+		return data;
+	});
+	*/
 	const updateQuery = (updater) => {
 		replace({
 			pathname,

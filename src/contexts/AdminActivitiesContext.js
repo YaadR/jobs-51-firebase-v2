90ce03a03e1a3302ff745ae0355cf16393115291
Y Patterns <y.code.patterns@gmail.com>
2022-01-11 21:23:41 +0200

    Resolve 2

--- src/contexts/AdminActivitiesContext.js
+++ src/contexts/AdminActivitiesContext.js
@@ -1,14 +1,28 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
+import { useQuery, useQueryClient } from "react-query";
 import { useHistory } from "react-router-dom";
 import useToggle from "../hooks/general/useToggle";
 import qs from "query-string";
+import getActivities from "../lib/API/queries/getActivities";
 
-export const AdminUsersContext = createContext();
+export const AdminActivitiesContext = createContext();
 
-export const AdminUsersProvider = ({ children }) => {
+export const AdminActivitiesProvider = ({ children }) => {
+	const queryClient = useQueryClient();
+  // const [hasMoreResults, setHasMoreResults] = useState(false)
+	const [lastResult, setLastResult] = useState(null);
 	const [isOpen, toggleOpen] = useToggle();
 	const { replace, pathname, location } = useHistory();
 	const query = qs.parse(location.search);
+	const hookRef = useQuery(["activities", query], async () => {
+		const res = await getActivities(query, lastResult);
+		setLastResult(res?.lastResult);
+		const oldData = queryClient.getQueryData(["activities", query]);
+		const data = Array.isArray(oldData) ? [...oldData, ...res.data] : res.data;
+
+		data?.forEach((a) => queryClient.setQueryData(["user-activity", a?.id], a));
+		return data;
+	});
 
 	const updateQuery = (updater) => {
 		replace({
@@ -18,15 +32,16 @@ export const AdminUsersProvider = ({ children }) => {
 	};
 
 	return (
-		<AdminUsersContext.Provider
+		<AdminActivitiesContext.Provider
 			value={{
 				isOpen,
 				toggleOpen,
 				query,
 				updateQuery,
+				...hookRef,
 			}}
 		>
 			{children}
-		</AdminUsersContext.Provider>
+		</AdminActivitiesContext.Provider>
 	);
 };
